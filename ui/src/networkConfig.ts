2281365735b274a42a0fed3a3ded16513ec9dd68
beratkarani <zlfrantic3@gmail.com>
2025-10-21 00:24:42 +0300

    Complete all the TODOs

--- ui/src/networkConfig.ts
+++ ui/src/networkConfig.ts
@@ -14,7 +14,7 @@ import { createNetworkConfig } from "@mysten/dapp-kit";
  * │ Modules: arena, hero, marketplace
  * └──
  */
-const PACKAGE_ID = "";
+const PACKAGE_ID = "0xf81487566b15b277ed73ba35aa76e5664d92768d3d6828b05339b42f4f166e41";
 
 const { networkConfig, useNetworkVariable, useNetworkVariables } =
   createNetworkConfig({
